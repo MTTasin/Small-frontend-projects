1deec1e721e61350ba573a1b98841faf392f9e1b
M.T. Tasin <m.t.tasin20@gmail.com>
2024-12-24 17:16:58 +0600

    Ready for deployment

--- src/Components/QR-Code-generator/QR-Code.jsx
+++ src/Components/QR-Code-generator/QR-Code.jsx
@@ -9,7 +9,9 @@ export default function QRCodeGenerator() {
             <h1 className="text-3xl font-bold text-blue-500 mb-5">QR Code Generator</h1>
             <input className="border-2 border-blue-500 p-2 rounded-xl" type="text" name="link" onChange={(e) => setLink(e.target.value)} value={link} placeholder="eg: https://google.com" id="link" />
             <h2 className="text-2xl font-bold text-blue-500 mt-5">QRCode for</h2><span>{link}</span>
-            {link ? <QRCode value={link} className="qrcode border-2 border-blue-500 rounded-xl mx-auto mt-2" /> : null}
+            <div className="w-full">
+            {link ? <QRCode value={link} className="qrcode mx-auto mt-2" /> : null}
+            </div>
         </div>
     )
 
